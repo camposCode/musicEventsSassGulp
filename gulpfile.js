2f7b94016eb995d848b9c99f6ecfb5dce36abdd3
Jose Campos <camposplaza77@gmail.com>
2022-12-07 19:04:44 +0100

    webp installed and set

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
+const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
+
+//Imagenes
+const webp = require('gulp-webp');
+
+function css(done){
+    src('src/scss/**/*')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(dest('build/css'));
+
+    done();
+}
+
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function dev(done){
+    watch('src/scss/**/*.scss', css);
+
+    done();
+}
+
+exports.css = css;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
